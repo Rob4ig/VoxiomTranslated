d45d963933c293f896c097fb72a33698bc394cdf
TheMasterRob4ig <136381220+TheMasterRob4ig@users.noreply.github.com>
2023-08-15 16:11:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
-fetch('translate.json')
-  .then(response => response.json())
-  .then(data => {
-    // Используйте данные из translate.json здесь
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Ошибка загрузки translate.json:', error);
-  })
-// function updateLinks() {
-//     let a = document.getElementsByTagName('a')
-//     const url = new URL(window.location.href)
-//     const playerName = url.pathname.split('/').pop()
-//     const paths = [
-//         "/player/" + playerName,
-//         "/player/" + playerName + "/ctg",
-//         "/player/" + playerName + "/br"
-//     ];
+function updateLinks() {
+    let a = document.getElementsByTagName('a')
+    const url = new URL(window.location.href)
+    const playerName = url.pathname.split('/').pop()
+    const paths = [
+        "/player/" + playerName,
+        "/player/" + playerName + "/ctg",
+        "/player/" + playerName + "/br"
+    ];
 
-//     for (let i = 0; i < a.length; i++) {
-//         let href = a[i].getAttribute('href')
+    for (let i = 0; i < a.length; i++) {
+        let href = a[i].getAttribute('href')
 
-//         // main /leaderboard
-//         const leaderboardLink = document.querySelector("a.YECSn[href='/leaderboard/ctg']")
-//         if (leaderboardLink) {
-//             leaderboardLink.textContent = "Лидеры"
-//         }
+        // main /leaderboard
+        const leaderboardLink = document.querySelector("a.YECSn[href='/leaderboard/ctg']")
+        if (leaderboardLink) {
+            leaderboardLink.textContent = "Лидеры"
+        }
 
-//         // /player
-//         if (href && paths.includes(href)) {
-//             if (href.endsWith(`/ctg`)) {
-//                 a[i].textContent = 'Захват Кристаллов'
-//             }
-//             if (href.endsWith(`/br`)) {
-//                 a[i].textContent = 'Королевская Битва'
-//             }
-//         }
+        // /player
+        if (href && paths.includes(href)) {
+            if (href.endsWith(`/ctg`)) {
+                a[i].textContent = 'Захват Кристаллов'
+            }
+            if (href.endsWith(`/br`)) {
+                a[i].textContent = 'Королевская Битва'
+            }
+        }
 
-//         // other
-//         if (linkInfo.hasOwnProperty(href)) {
-//             a[i].innerHTML = '';
-//             if (linkInfo[href].hasOwnProperty('imgSrc')) {
-//                 let img = document.createElement('img')
-//                 img.src = linkInfo[href].imgSrc
-//                 a[i].appendChild(img)
-//             }
-//             a[i].appendChild(document.createTextNode(linkInfo[href].text))
-//         }
-//     }
-// }
-// updateLinks()
-// setInterval(updateLinks, 100)
+        // other
+        if (linkInfo.hasOwnProperty(href)) {
+            a[i].innerHTML = '';
+            if (linkInfo[href].hasOwnProperty('imgSrc')) {
+                let img = document.createElement('img')
+                img.src = linkInfo[href].imgSrc
+                a[i].appendChild(img)
+            }
+            a[i].appendChild(document.createTextNode(linkInfo[href].text))
+        }
+    }
+}
+updateLinks()
+setInterval(updateLinks, 100)
