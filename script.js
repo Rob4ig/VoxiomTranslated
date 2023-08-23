494323a31f494ccef3b98282a9a4e977fda865d0
TheMasterRob4ig <136381220+TheMasterRob4ig@users.noreply.github.com>
2023-08-23 18:35:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ function trUpdate(words) {
         document.querySelectorAll(classes).forEach(element => {
             for (let key in translations) {
                 if (element.textContent.trim() === key) {
-                    element.textContent = translations[key]
+                    const translationData = translations[key]
+                    element.textContent = translationData
+                    if (typeof translationData === 'object') {
+                        const imgElement = document.createElement('img')
+                        imgElement.src = translationData.src
+                        const textElement = document.createElement('span')
+                        textElement.textContent = translationData.text
+                        element.innerHTML = ''
+                        element.appendChild(imgElement)
+                        element.appendChild(textElement)
+                    }
                 }
             }
         })
@@ -12,4 +22,14 @@ function trUpdate(words) {
 }
 fetch('https://raw.githubusercontent.com/TheMasterRob4ig/VoxiomTranslated/main/tr.json')
     .then(response => response.json())
-    .then(data => setInterval(() => trUpdate(data.translations), 100))
+    .then(data => {
+        trUpdate(data.translations)
+        setInterval(() => trUpdate(data.translations), 100)
+    })
+fetch("https://raw.githubusercontent.com/TheMasterRob4ig/VoxiomTranslated/main/style.css")
+    .then(response => response.text())
+    .then(data => {
+        const styleElement = document.createElement("style")
+        styleElement.textContent = data
+        document.head.appendChild(styleElement)
+    })
